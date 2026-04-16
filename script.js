a0deac7fbd32f115284b7eeb6092e969e2752d71
Mahelys307 <fouillotmarsanmahelys@gmail.com>
2026-04-16 07:59:16 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
 const menuData = {
     items: [
+        }
+  function search() {
+    const query = document.getElementById("searchInput").value;
+    if (query) {
+      alert("Tu as recherché : " + query);
+      // Exemple : redirection vers Google
+      // window.location.href = "https://www.google.com/search?q=" + encodeURIComponent(query);
+    }
         { text: "Accueil", url: "index.html" },
         {
             text: "Vie scolaire",
